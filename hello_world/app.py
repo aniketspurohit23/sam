00cdd3008bce3a08add41a8a1437b7a0549caab5
aniketspurohit23 <aniket.purohit@aptusdatalabs.com>
2019-11-06 16:56:30 +0530

    random stuff

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Aniket! Nice to meet you!!",
+            "message": "I am samaritan",
             # "location": ip.text.replace("\n", "")
         }),
     }
