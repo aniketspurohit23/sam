7cc957e5a47a917eeb3a2f2d04d344c8b9bff9f4
aniketspurohit23 <aniket.purohit@aptusdatalabs.com>
2019-11-06 16:41:31 +0530

    now saying hello to aniket

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Sarthak! Nice to meet you!!",
+            "message": "hello Aniket! Nice to meet you!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
